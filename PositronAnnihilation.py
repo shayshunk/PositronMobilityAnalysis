d319d59431d0adff5ced0a1971f6717a1d126e99
shayshunk <60236335+shayshunk@users.noreply.github.com>
2023-12-27 00:22:24 -0500

    Added comments to the code.

--- PositronAnnihilation.py
+++ PositronAnnihilation.py
@@ -3,11 +3,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-with h5py.File('Run_4.h5', 'r') as hdf:
+# Open HDF5 file and extract datasets and tables
+with h5py.File('Final_Run.h5', 'r') as hdf:
     ls = list(hdf.keys())
     print("List of datasets:\n", ls)
+
     NCapt = hdf.get('NCapt')[()]
-    #print(NCapt)
     Prim = hdf.get('Prim')[()]
 
     posCaptT = NCapt['t']
@@ -27,57 +28,67 @@ with h5py.File('Run_4.h5', 'r') as hdf:
     IBDPID = Prim['PID']
     IBDevents = Prim['evt']
 
+# Two dataframes to contain the two tables. Note that y and z are flipped
 posPD = pd.DataFrame({'evt':eventNos, 'time':posCaptT, 'gammas':posGamma, 'Egammas':posGammaE, 'Pos x':posLocsX, 'Pos y':posLocsY, 'Pos z':posLocsZ, 'vol':posVol})
 IBDPD = pd.DataFrame({'vol':IBDVol, 'evt':IBDevents, 'PID':IBDPID, 'x':IBDLocsX, 'y':IBDLocsZ, 'z':IBDLocsY})
 
+# Setting index for ease of use
 posPD.set_index('evt')
 IBDPD.set_index('evt')
 
+# Primaries table has z backwards for some reason
 IBDPD['z'] = IBDPD['z'] * -1
 
+# Dropping events out of segments
 posPD = posPD[posPD.vol >= 0]
 posPD = posPD[posPD.vol <= 153]
-print(posPD.shape)
+IBDPD = IBDPD[IBDPD.vol >= 0]
+IBDPD = IBDPD[IBDPD.vol <= 153]
+
+# Dropping events that aren't proper positron annihilations
 posPD = posPD[posPD.time < 0.15]
-print(posPD.shape)
 posPD = posPD[posPD.gammas == 2]
 posPD = posPD[posPD.Egammas == 1.02199782]
 
-IBDPD = IBDPD[IBDPD.vol >= 0]
-IBDPD = IBDPD[IBDPD.vol <= 153]
+# Dropping all rows in IBD dataframe that aren't positrons
 IBDPD = IBDPD[IBDPD.PID == -11]
 
+# Aligning both tables to drop the same events
 IBDPD = IBDPD[IBDPD.evt.isin(posPD.evt)]
 posPD = posPD[posPD.evt.isin(IBDPD.evt)]
 
-posPD = posPD.reset_index(drop=True)
-IBDPD = IBDPD.reset_index(drop=True)
-
+# Extracting columns to combine into one dataframe
 IBDx = IBDPD['x']
 IBDy = IBDPD['y']
 IBDz = IBDPD['z']
 IBDevts = IBDPD['evt']
 
+# Don't need this anymore
 posPD = posPD.drop('Egammas', axis = 1)
 posPD = posPD.drop('gammas', axis = 1)
 posPD = posPD.drop('time', axis = 1)
 posPD = posPD.drop('vol', axis = 1)
 
+# Combining two dataframes
 posPD = pd.concat([posPD, IBDx.rename('IBD x')], axis = 1)
 posPD = pd.concat([posPD, IBDy.rename('IBD y')], axis = 1)
 posPD = pd.concat([posPD, IBDz.rename('IBD z')], axis = 1)
 posPD = pd.concat([posPD, IBDevts.rename('IBD evt')], axis = 1)
 
+# Y is offset so fixing that by centering the range around 0 
 yRange = (posPD['IBD y'].max() - posPD['IBD y'].min()) / 2
 posPD['IBD y'] = posPD['IBD y'] + (yRange - posPD['IBD y'].max())
 
+# Adding columns for each differece
 posPD['X difference'] = posPD['Pos x'] - posPD['IBD x']
 posPD['Y difference'] = posPD['Pos y'] - posPD['IBD y']
 posPD['Z difference'] = posPD['Pos z'] - posPD['IBD z']
 
-print(posPD.loc[posPD['evt'] != posPD['IBD evt']])
-print(posPD.iloc[65890:65910])
+# Checking if there are still misalignments
+print("If there are any misaligned events, it will print now:", posPD.loc[posPD['evt'] != posPD['IBD evt']])
+posPD.drop('IBD evt', axis=1)
 
+# Cutting a few mm (30) inside the active detector. Only applied to IBDs
 posPD = posPD[posPD['IBD y'] < 773]
 posPD = posPD[posPD['IBD y'] > -773]
 #posPD = posPD[posPD['Pos y'] < 803]
@@ -93,31 +104,30 @@ posPD = posPD[posPD['IBD x'] > -992]
 #posPD = posPD[posPD['Pos x'] < 1022]
 #posPD = posPD[posPD['Pos x'] > -1022]
 
+# Aligning tables again just for safety
 IBDPD = IBDPD[IBDPD.evt.isin(posPD.evt)]
 posPD = posPD[posPD.evt.isin(IBDPD.evt)]
 
-
+# Make counting easier by resetting indices
 posPD = posPD.reset_index(drop=True)
 IBDPD = IBDPD.reset_index(drop=True)
 
+# Adding difference columns
 posPD['X difference'] = posPD['Pos x'] - posPD['IBD x']
 posPD['Y difference'] = posPD['Pos y'] - posPD['IBD y']
 posPD['Z difference'] = posPD['Pos z'] - posPD['IBD z']
 
-print(posPD.tail(20))
+# Printing preview of dataframe to terminal
+print(posPD.head(20))
 
-print("Range of IBD Y: ", posPD['IBD y'].min(), "to", posPD['IBD y'].max())
-print("Range of Pos Y: ", posPD['Pos y'].min(), "to", posPD['Pos y'].max())
-
+# Gathering the average difference in each direction and the average total difference
 x_dif = posPD['X difference'].mean()
 y_dif = posPD['Y difference'].mean()
 z_dif = posPD['Z difference'].mean()
 total_diff = np.sqrt(x_dif*x_dif + y_dif*y_dif + z_dif*z_dif)
 
+# Printing to terminal
 print("Average X difference: ", x_dif)
 print("Average Y difference: ", y_dif)
 print("Average Z difference: ", z_dif)
 print("Average total vector: ", total_diff)
-
-plt.hist(posPD['IBD z'])
-#plt.show()
