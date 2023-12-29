102ce57989b14360d0a1ec2eee64a398ff03ff05
shayshunk <60236335+shayshunk@users.noreply.github.com>
2023-12-28 23:31:02 -0500

    Added vector of positron to IBD, projected onto the IBD to reactor core vector.

--- PositronAnnihilation.py
+++ PositronAnnihilation.py
@@ -28,9 +28,13 @@ with h5py.File('Final_Run.h5', 'r') as hdf:
     IBDPID = Prim['PID']
     IBDevents = Prim['evt']
 
-# Two dataframes to contain the two tables. Note that y and z are flipped
+# Two dataframes to contain the two tables. Note that y and z are flipped for IBDs
 posPD = pd.DataFrame({'evt':eventNos, 'time':posCaptT, 'gammas':posGamma, 'Egammas':posGammaE, 'Pos x':posLocsX, 'Pos y':posLocsY, 'Pos z':posLocsZ, 'vol':posVol})
 IBDPD = pd.DataFrame({'vol':IBDVol, 'evt':IBDevents, 'PID':IBDPID, 'x':IBDLocsX, 'y':IBDLocsZ, 'z':IBDLocsY})
+
+# Extract reactor core position
+reactorPos = np.array([IBDPD.at[0, 'x'], IBDPD.at[0, 'y'], IBDPD.at[0, 'z']])
+print("Original reactor position (mm): \nx =", reactorPos[0], "\ny =", reactorPos[1], "\nz = ", reactorPos[2])
 
 # Setting index for ease of use
 posPD.set_index('evt')
@@ -38,6 +42,7 @@ IBDPD.set_index('evt')
 
 # Primaries table has z backwards for some reason
 IBDPD['z'] = IBDPD['z'] * -1
+reactorPos[2] = reactorPos[2] * -1
 
 # Dropping events out of segments
 posPD = posPD[posPD.vol >= 0]
@@ -46,9 +51,10 @@ IBDPD = IBDPD[IBDPD.vol >= 0]
 IBDPD = IBDPD[IBDPD.vol <= 153]
 
 # Dropping events that aren't proper positron annihilations
-posPD = posPD[posPD.time < 0.15]
+posPD = posPD[posPD.time < 0.14]
 posPD = posPD[posPD.gammas == 2]
 posPD = posPD[posPD.Egammas == 1.02199782]
+posPD = posPD.drop_duplicates(subset=['evt'])
 
 # Dropping all rows in IBD dataframe that aren't positrons
 IBDPD = IBDPD[IBDPD.PID == -11]
@@ -67,7 +73,7 @@ IBDy = IBDPD['y']
 IBDz = IBDPD['z']
 IBDevts = IBDPD['evt']
 
-# Don't need this anymore
+# Don't need these columns anymore
 posPD = posPD.drop('Egammas', axis = 1)
 posPD = posPD.drop('gammas', axis = 1)
 posPD = posPD.drop('time', axis = 1)
@@ -81,7 +87,10 @@ posPD = pd.concat([posPD, IBDevts.rename('IBD evt')], axis = 1)
 
 # Y is offset so fixing that by centering the range around 0 
 yRange = (posPD['IBD y'].max() - posPD['IBD y'].min()) / 2
-posPD['IBD y'] = posPD['IBD y'] + (yRange - posPD['IBD y'].max())
+yMax = posPD['IBD y'].max()
+posPD['IBD y'] = posPD['IBD y'] + (yRange - yMax)
+reactorPos[1] = reactorPos[1] + (yRange - yMax)
+print("Original reactor position (mm): \nx =", reactorPos[0], "\ny =", reactorPos[1], "\nz = ", reactorPos[2])
 
 # Adding columns for each differece
 posPD['X difference'] = posPD['Pos x'] - posPD['IBD x']
@@ -89,22 +98,22 @@ posPD['Y difference'] = posPD['Pos y'] - posPD['IBD y']
 posPD['Z difference'] = posPD['Pos z'] - posPD['IBD z']
 
 # Checking if there are still misalignments
-print("If there are any misaligned events, it will print now:", posPD.loc[posPD['evt'] != posPD['IBD evt']])
+print("If there are any misaligned events, it will print now:\n", posPD.loc[posPD['evt'] != posPD['IBD evt']])
 posPD.drop('IBD evt', axis=1)
 
 # Cutting a few mm (30) inside the active detector. Only applied to IBDs
-posPD = posPD[posPD['IBD y'] < 773]
-posPD = posPD[posPD['IBD y'] > -773]
+#posPD = posPD[posPD['IBD y'] < 773]
+#posPD = posPD[posPD['IBD y'] > -773]
 #posPD = posPD[posPD['Pos y'] < 803]
 #posPD = posPD[posPD['Pos y'] > -803]
 
-posPD = posPD[posPD['IBD z'] < 557]
-posPD = posPD[posPD['IBD z'] > -557]
+#posPD = posPD[posPD['IBD z'] < 557]
+#posPD = posPD[posPD['IBD z'] > -557]
 #posPD = posPD[posPD['Pos z'] < 587]
 #posPD = posPD[posPD['Pos z'] > -587]
 
-posPD = posPD[posPD['IBD x'] < 992]
-posPD = posPD[posPD['IBD x'] > -992]
+#posPD = posPD[posPD['IBD x'] < 1019]
+#posPD = posPD[posPD['IBD x'] > -1019]
 #posPD = posPD[posPD['Pos x'] < 1022]
 #posPD = posPD[posPD['Pos x'] > -1022]
 
@@ -117,9 +126,17 @@ posPD = posPD.reset_index(drop=True)
 IBDPD = IBDPD.reset_index(drop=True)
 
 # Adding difference columns
-posPD['X difference'] = posPD['Pos x'] - posPD['IBD x']
-posPD['Y difference'] = posPD['Pos y'] - posPD['IBD y']
-posPD['Z difference'] = posPD['Pos z'] - posPD['IBD z']
+posPD['X difference'] = posPD['IBD x'] - posPD['Pos x']
+posPD['Y difference'] = posPD['IBD y'] - posPD['Pos y']
+posPD['Z difference'] = posPD['IBD z'] - posPD['Pos z']
+
+# Getting vector from IBD to reactor core for each event
+reactorVectorx = reactorPos[0] - posPD['IBD x']
+reactorVectory = reactorPos[1] - posPD['IBD y']
+reactorVectorz = reactorPos[2] - posPD['IBD z']
+
+# Getting projected vector for positron annihilation along IBD to reactor core
+projectedVector = (reactorVectorx * posPD['X difference'] + reactorVectory * posPD['Y difference'] + reactorVectorz * posPD['Z difference']) / np.sqrt(reactorVectorx * reactorVectorx + reactorVectory * reactorVectory + reactorVectorz * reactorVectorz)
 
 # Printing preview of dataframe to terminal
 print(posPD.head(20))
@@ -129,9 +146,11 @@ x_dif = posPD['X difference'].mean()
 y_dif = posPD['Y difference'].mean()
 z_dif = posPD['Z difference'].mean()
 total_diff = np.sqrt(x_dif*x_dif + y_dif*y_dif + z_dif*z_dif)
+avgProjVector = projectedVector.mean()
 
 # Printing to terminal
 print("Average X difference: ", x_dif)
 print("Average Y difference: ", y_dif)
 print("Average Z difference: ", z_dif)
 print("Average total vector: ", total_diff)
+print("Average total vector projected along the vector from the IBD location towards the reactor core: ", avgProjVector)
